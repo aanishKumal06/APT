28a688219a3eaa5948ee45e889246e34ced48010
ayushgrg0 <gurungayush982@gmail.com>
2025-04-20 09:47:04 +0545

    Update DatabaseConnection.java

--- src/main/java/com/marshmallowhaven/DatabaseConnection/DatabaseConnection.java
+++ src/main/java/com/marshmallowhaven/DatabaseConnection/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 	
 	private static String databaseName = "marshmallow_haven"; 
-	private static String username = "anish123";
-	private static String password = "password@123"; 
+	private static String username = "root";
+	private static String password = ""; 
 	private static String jdbcURL = "jdbc:mysql://localhost:3306/" + databaseName;
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
